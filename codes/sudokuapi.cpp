2b6a60e8d20f2968aabac3efd09d0562ec87e4f2
Hakan Emre Kartal <54192616+AIntelligent@users.noreply.github.com>
2024-04-12 23:27:51 +0300

    Add files via upload

--- codes/sudokuapi.cpp
+++ codes/sudokuapi.cpp
@@ -1,22 +1,25 @@
 /*
  * SUDOKUAPI.CPP
- * Hakan Emre KARTAL
- * hek@nula.com.tr
- * Iskenderun/HATAY
  *
- * 04/04/2006
- *    o Üretildi
+ * 10/04/2024
+ *    o Bazý kiþisel bilgiler güncellendi.
+ *    o Borland C++Builder 6.0 ile yeniden derlendi.
  * 23/04/2006
  *    o Bazý düzeltmeler eklendi.
  *    o Sudoku harita üreteci rastgele sayý üreteç deðeri deðiþtirildi.
  *    o Sol dikey baþlýk çubuðu rengi ve durum çubuðu renkleri deðiþtirildi.
  *    o Son þekli verildi.
- * 10/04/2024
- *    o Bazý kiþisel bilgiler güncellendi.
- *    o Borland C++Builder 6.0 ile yeniden derlendi.
+ * 04/04/2006
+ *    o Üretildi
+ *
+ * Hakan Emre KARTAL tarafýndan Ýskenderun/HATAY'da 04/04/2006 tarihinde
+ * Borland C++Builder 6.0 ile geliþtirildi.
  *
  * Written by Hakan Emre KARTAL in Iskenderun/HATAY on 04/04/2006,
- * developed for Borland C++Builder 6.0
+ * developed with Borland C++Builder 6.0
+ *
+ * Hakan Emre KARTAL
+ * hek@nula.com.tr
  */
 #include "sudokuapi.h"
 
